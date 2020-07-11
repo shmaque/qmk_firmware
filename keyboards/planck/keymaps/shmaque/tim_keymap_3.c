7665b1e7d82539ac12f9e5e7731c73b39083157c
shmaque <tchermak@gmail.com>
2020-07-10 23:19:32 -0400

    added visualizations

--- keyboards/planck/keymaps/shmaque/tim_keymap_3.c
+++ keyboards/planck/keymaps/shmaque/tim_keymap_3.c
@@ -2,35 +2,85 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   /* Primary layer */
+   /* Primary layer (QWERTY)
+    * ,-----------------------------------------------------------------------------------.
+    * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | Ctrl | Alt  | GUI  |  Fn  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+    * `-----------------------------------------------------------------------------------'
+    */
    [0] = LAYOUT_ortho_4x12(
       KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, 
       KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, 
       KC_LCTL, KC_LALT, KC_LGUI, MO(5),   MO(2),   KC_SPC,  KC_SPC,  MO(3),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
    ),
-   /* Lower (Numrow, etc...) */
+   /* Lower (Numrow, etc...) 
+    * ,-----------------------------------------------------------------------------------.
+    * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | TRNS |      |      |      |      |      |      |      |      |      |      | TRNS |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | NumPd|      |      |      |      |      |      |      |      |      |      | TRNS |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | TRNS | TRNS | TRNS | TRNS | TRNS |    TRNS     | MO(6)| TRNS | TRNS | TRNS | TRNS |
+    * `-----------------------------------------------------------------------------------'
+    */
    [2] = LAYOUT_ortho_4x12(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
       KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, 
       MO(4),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, 
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(6),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
    ),
-   /* Raise (symbols/shift keys, etc...) */
+   /* Raise (symbols/shift keys, etc...)
+    * ,-----------------------------------------------------------------------------------.
+    * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | TRNS |      |      |      |      |      |   -  |   +  |      |   [  |   ]  |   \  |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | Shift|      |      |      |      |      |   _  |   =  |      |   {  |   }  | TRNS |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | TRNS | TRNS | TRNS | TRNS | MO(6)|    TRNS     | TRNS | TRNS | TRNS | TRNS | TRNS |
+    * `-----------------------------------------------------------------------------------'
+    */
    [3] = LAYOUT_ortho_4x12(
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, 
-      KC_TAB,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PMNS, KC_PLUS, KC_NO,   KC_LBRC, KC_RBRC, KC_BSLS, 
+      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PMNS, KC_PLUS, KC_NO,   KC_LBRC, KC_RBRC, KC_BSLS, 
       KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_EQL,  KC_NO,   KC_LCBR, KC_RCBR, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(6),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
    ),
-   /* Numpad Layer */
+   /* Numpad Layer
+    * ,-----------------------------------------------------------------------------------.
+    * |      |      |      |      |      |      |      |      |   7  |   8  |   9  |  -   |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * |      |      |      |      |      |      |      |      |   4  |   5  |   6  |  +   |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * |      |      |      |      |      |      |      |      |   1  |   2  |   3  | ENT  |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * | TRNS | TRNS | TRNS | TRNS | TRNS |    TRNS     |      |   =  |   0  |   .  |  *   |
+    * `-----------------------------------------------------------------------------------'
+    */
    [4] = LAYOUT_ortho_4x12(
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, 
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_PEQL, KC_P0,   KC_PDOT, KC_PAST
    ),
-   /* Fn layer (F keys, pgupdown/etc...) */
+   /* Fn layer (F keys, pgupdown/etc...)
+    * ,-----------------------------------------------------------------------------------.
+    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F0  |  Del |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * |      |  F11 |  F12 |      |      |      |      |      | PrSc | ScLk | Pause|  Ins |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * |      |      |      |      |      |      |      |      |      |      |      |      |
+    * |------+------+------+------+------+------+------+------+------+------+------+------|
+    * |      |      |      |      |      |             |      | Home | PgUp | PgDn | End  |
+    * `-----------------------------------------------------------------------------------'
+    */
    [5] = LAYOUT_ortho_4x12(
       KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, 
       KC_NO,   KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS, KC_INS, 
