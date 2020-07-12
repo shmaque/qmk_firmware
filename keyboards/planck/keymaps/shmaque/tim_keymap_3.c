96b00a9cd2f2ef9ac5e01730047a1dd8501aa65a
shmaque <tchermak@gmail.com>
2020-07-12 14:21:51 -0400

    add numlock key to numpad layer just in case

--- keyboards/planck/keymaps/shmaque/tim_keymap_3.c
+++ keyboards/planck/keymaps/shmaque/tim_keymap_3.c
@@ -59,15 +59,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------+------+------+------+------+------+------|
     * |      |      |      |      |      |      |      |      |   4  |   5  |   6  |  +   |
     * |------+------+------+------+------+------+------+------+------+------+------+------|
-    * |      |      |      |      |      |      |      |      |   1  |   2  |   3  | ENT  |
+    * |      |      |      |      |      |      | NumLk|      |   1  |   2  |   3  | ENT  |
     * |------+------+------+------+------+------+------+------+------+------+------+------|
-    * | TRNS | TRNS | TRNS | TRNS | TRNS |    TRNS     |      |   =  |   0  |   .  |  *   |
+    * | TRNS | TRNS | TRNS | TRNS |      |    TRNS     |      |   =  |   0  |   .  |  *   |
     * `-----------------------------------------------------------------------------------'
     */
    [4] = LAYOUT_ortho_4x12(
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, 
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NLCK, KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_PEQL, KC_P0,   KC_PDOT, KC_PAST
    ),
    /* Fn layer (F keys, pgupdown/etc...)
